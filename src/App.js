4b61f7ae5afbfeaa551f362417f888a7bf4e427e
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-30 10:16:07 +0530

    update: inital setup of the app with everything configured

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+     <h1 className="text-red-500 text-center font-bold">configured with tailwindcss</h1>
+    </div>
+  );
+}
+
+export default App;
