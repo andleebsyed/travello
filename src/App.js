b46736251f304b451043cb96297ede416881169d
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-11 18:37:18 +0530

    bug: trying to fix a bug in application of classes from tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
+// import "./App.css";
 import { Homepage } from "./common/pages/homepage/homepage";
 import { Landing } from "./common/pages/landing/landing";
+import { Navbar } from "./common/pages/navbar/navbar";
 import { Login } from "./features/users/login/login";
 import { Signup } from "./features/users/signup/signup";
+import { setUpAuthHeaderForServiceCalls } from "./services/users/users";
 function App() {
+  useEffect(() => {
+    console.log("mainpage useeffect ran");
+    setUpAuthHeaderForServiceCalls(localStorage.getItem("token"));
+  }, []);
   return (
-    <div>
+    <div className=" ">
+      {/* <div className=""> */}
+
+      {/* </div> */}
+
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<Homepage />} />
       </Routes>
+      {/* <div></div> */}
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,42 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  mode: "jit",
+  purge: ["./src/**/**/**/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-        colors: {
+      colors: {
         blue: {
-        xlight: '#162d40',
-        light: "#1DA1F2", 
-        DEFAULT: "#15202B"
-      },
-      white: {
-        DEFAULT: "#FFFFFF"
+          xlight: "#162d40",
+          light: "#1DA1F2",
+          DEFAULT: "#15202B",
+        },
+        white: {
+          DEFAULT: "#FFFFFF",
         },
         red: {
-        DEFAULT: "#DC2626"
-      }
-    },
+          DEFAULT: "#DC2626",
+        },
+      },
       screens: {
-        'xsm': '550px',
-        'flexbreak': "1080px",
-        'gridbreak': '950px'
+        xsm: "550px",
+        flexbreak: "1080px",
+        gridbreak: "950px",
       },
       gridTemplateColumns: {
-          //for homepage 
-        'home': '1fr 3fr 1fr',
-        'home-middle': 'minmax(0px,1fr) 3fr',
-        'home-mobile': '1fr'
+        //for homepage
+        home: "1fr 3fr 1fr",
+        "home-middle": "minmax(0px,1fr) 3fr",
+        "home-mobile": "1fr",
       },
       gridTemplateRows: {
-           //for homepage 
-        'home': '1fr 3fr',
-        'home-mobile': '1fr 2fr 1fr'
-        }
+        //for homepage
+        home: "1fr 3fr",
+        "home-mobile": "1fr 2fr 1fr",
+      },
     },
-  
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
