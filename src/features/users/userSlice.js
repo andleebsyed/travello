d69ba990ca18c8bc324371560af575ebcae71b0d
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-11 23:03:45 +0530

    update: added a property authorized to user to check at any place if user is authorized or not

--- src/features/users/userSlice.js
+++ src/features/users/userSlice.js
@@ -1,15 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
-
 export const userSlice = createSlice({
-    name: "userSlice",
+  name: "userSlice",
+  initialState: {
     status: "idle",
     error: null,
-    userDetails: [],
-    reducers: {
-        
-    },
-    extraReducers: {
-
-    }
-})
-
+    authorized: localStorage.getItem("token") ? true : false,
+  },
+  reducers: {},
+  extraReducers: {},
+});
+export const { userAuth } = userSlice.actions;
+export default userSlice.reducer;
