0d425157f1f2d319e7e2e470e7d28e3c38f3324b
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-11 18:39:44 +0530

    update: made an api call to server using  createAsyncThunk to fetch posts from backend and populate it in state

--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -1,14 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { FetchAllPosts } from "../../services/posts/posts";
 const initialState = {
   status: "idle",
   error: null,
   posts: [],
 };
+export const loadPosts = createAsyncThunk("user/posts", async () => {
+  const response = await FetchAllPosts();
+  console.log("response in thunk ", { response });
+  return response.ourUser.posts;
+});
 export const postSlice = createSlice({
   name: "postSlice",
   initialState,
   reducers: {},
-  extraReducers: {},
+  extraReducers: {
+    [loadPosts.pending]: (state) => {
+      state.status = "loading";
+    },
+    [loadPosts.fulfilled]: (state, action) => {
+      state.posts = action.payload;
+      state.status = "success";
+    },
+    [loadPosts.rejected]: (state, action) => {
+      state.status = "error";
+      state.error = action.error.message;
+    },
+  },
 });
 
 export default postSlice.reducer;
