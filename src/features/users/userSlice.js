a11d517fb38f6b7704fc4783b9b44ec546d30e43
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-11 18:37:49 +0530

    crated a slice for user data

---
+++ src/features/users/userSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+    name: "userSlice",
+    status: "idle",
+    error: null,
+    userDetails: [],
+    reducers: {
+        
+    },
+    extraReducers: {
+
+    }
+})
+
