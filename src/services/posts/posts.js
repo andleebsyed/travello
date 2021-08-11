249496f8ee90a6f7f5e4f77d61a0a3b678d0ff82
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-11 18:42:41 +0530

    update : made a  function which will call backend api and gather all the posts of the said authorized user

--- src/services/posts/posts.js
+++ src/services/posts/posts.js
@@ -13,3 +13,16 @@ export async function PostCreation(newPostContent) {
   console.log({ response });
   return response.data;
 }
+
+export async function FetchAllPosts() {
+  try {
+    const response = await axios.post(BASE_URL + "posts/read");
+    console.log({ response });
+    if (response.status === 200) {
+      console.log(response.data);
+      return response.data;
+    }
+  } catch (error) {
+    console.error("Error occurred while sending request to api ", error);
+  }
+}
