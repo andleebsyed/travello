ff40e393998c3204a82b2ac65a58fc25eba252ee
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-12 15:45:49 +0530

    style: made navbar to sit in ui perfectly on different screen sizes

--- src/common/pages/navbar/navbar.js
+++ src/common/pages/navbar/navbar.js
@@ -1,22 +1,53 @@
+import { NavLink } from "react-router-dom";
+
 export function Navbar() {
   return (
-    <main className="fixed  xsm:static  w-screen bottom-0  xsm:w-auto bg-blue  ">
-      <ul className="flex   justify-between xsm:flex-col xsm:justify-between items-end xsm:items-center gridbreak:items-start ">
+    <main className="fixed mt-12 xsm:sticky xsm:self-start w-screen bottom-0 xsm:top-0  xsm:pr-8 gridbreak:pr-0 xsm:w-auto bg-blue text-white  ">
+      <ul className="flex pr-4  justify-around xsm:flex-col xsm:justify-between items-end xsm:items-center gridbreak:items-start ">
         <li className="navbar-list-item  ">
-          <i className="ri-home-7-line text-3xl  self-center"></i>
-          <span className="navbar-list-item-name  ">Home</span>
+          <NavLink
+            to="/home"
+            activeClassName="active-class"
+            className="xsm:flex xsm:p-4 gridbreak:p-0 gridbreak:pl-4"
+            end
+          >
+            <i className="ri-home-7-line text-3xl  self-center "></i>
+            <span className="navbar-list-item-name  ">Home</span>
+          </NavLink>
         </li>
         <li className="navbar-list-item">
-          <i className="ri-hashtag text-3xl self-center fill"></i>
-          <span className="navbar-list-item-name ">Explore</span>
+          <NavLink
+            to="/explore"
+            activeClassName="active-class"
+            className="xsm:flex xsm:p-4 gridbreak:p-0 gridbreak:pl-4"
+            end
+          >
+            <i className="ri-hashtag text-3xl self-center fill"></i>
+
+            <span className="navbar-list-item-name ">Explore</span>
+          </NavLink>
         </li>
         <li className="navbar-list-item">
-          <i className="ri-notification-3-line text-3xl self-center"></i>
-          <span className="navbar-list-item-name ">Notifications</span>
+          <NavLink
+            to="/notifications"
+            activeClassName="active-class"
+            className="xsm:flex xsm:p-4 gridbreak:p-0 gridbreak:pl-4"
+            end
+          >
+            <i className="ri-notification-3-line text-3xl self-center"></i>
+            <span className="navbar-list-item-name ">Notifications</span>
+          </NavLink>
         </li>
         <li className="navbar-list-item">
-          <i className="ri-user-3-line text-3xl self-center"></i>
-          <span className="navbar-list-item-name ">Profile</span>
+          <NavLink
+            to="/profile"
+            activeClassName="active-class"
+            className="xsm:flex xsm:p-4 gridbreak:p-0 gridbreak:pl-4"
+            end
+          >
+            <i className="ri-user-3-line text-3xl self-center"></i>
+            <span className="navbar-list-item-name ">Profile</span>
+          </NavLink>
         </li>
       </ul>
     </main>
