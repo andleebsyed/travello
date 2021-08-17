473bb1d02efca3fd02f088794f767d8640313475
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-17 09:08:16 +0530

    update: made functions for different api calls related to comments of a post

--- src/services/posts/posts.js
+++ src/services/posts/posts.js
@@ -30,3 +30,33 @@ export async function LikeInteraction(data) {
   console.log({ response });
   return response.data;
 }
+
+export async function FetchComments({ postId }) {
+  console.log("coming in api call ", postId);
+  const response = await axios.post(BASE_URL + "posts/fetchcomments", {
+    postId,
+  });
+  if (response.status === 200) {
+    console.log("good response");
+    console.log(response.data);
+    return response.data;
+  }
+}
+
+export async function AddComment({ postId, content }) {
+  const response = await axios.post(BASE_URL + "posts/addcomment", {
+    postId,
+    content,
+  });
+  console.log("comment added or not ", { response });
+  return response.data;
+}
+
+export async function RemoveComment({ postId, commentId }) {
+  const response = await axios.post(BASE_URL + "posts/removecomment", {
+    postId,
+    commentId,
+  });
+  console.log("api response ", { response });
+  return response.data;
+}
