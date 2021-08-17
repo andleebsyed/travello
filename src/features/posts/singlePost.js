d25c72950a9c7948cafdbc4e077a8af16d5370e4
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-17 14:53:14 +0530

    update: added a go back option on a singlePost page

--- src/features/posts/singlePost.js
+++ src/features/posts/singlePost.js
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { setUpAuthHeaderForServiceCalls } from "../../services/users/users";
 import { loadPosts } from "./postSlice";
 import { ShowPost } from "./showPost";
-
+import { BiArrowBack } from "react-icons/bi";
 export function SinglePost() {
   const { posts } = useSelector((state) => state.posts);
   const { postId } = useParams();
   const dispatch = useDispatch();
-  const { name, username } = useSelector((state) => state.posts);
+  //   const { name, username } = useSelector((state) => state.posts);
   useEffect(() => {
     if (posts === null) {
       setUpAuthHeaderForServiceCalls(localStorage.getItem("token"));
@@ -19,10 +19,20 @@ export function SinglePost() {
   console.log({ postId });
   const post = posts?.find((post) => post._id === postId);
   console.log(post);
+  const navigate = useNavigate();
   return post === undefined ? (
     <div>loading </div>
   ) : (
-    <div className="w-[95vw] md:w-[70vw] xsm:mr-4 text-white">
+    <div className="w-screen min-h-screen md:w-[70vw] border border-opacity-20 xsm:mr-4 text-white">
+      <section className="bg-blue  p-2 border-b border-opacity-20  flex">
+        <button
+          onClick={() => navigate(-1)}
+          className="self-center text-blue-light hover:bg-blue-light hover:bg-opacity-20 rounded-full p-2"
+        >
+          <BiArrowBack size={18} />
+        </button>
+        <p className="p-2 text-xl">Post</p>
+      </section>
       <ShowPost post={post} />
     </div>
   );
