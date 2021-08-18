69def31496bb15ab84da2ee0d2d4a12c3b5c7e33
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-18 17:44:36 +0530

    refactor: removed unused code

--- src/features/posts/showPost.js
+++ src/features/posts/showPost.js
@@ -1,18 +1,13 @@
 import moment from "moment";
 import { useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
-// import { Link, useNavigate } from "react-router-dom";
-// import { FcLike } from "react-icons/fc";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { GoComment } from "react-icons/go";
-// import { Comments } from "./Comments";
 import { LikeInteraction } from "../../services/posts/posts";
-// import { startProgressBar, stopProgressBar } from "./postSlice";
 import { useState } from "react";
 import { Comments } from "./Comments";
 export function ShowPost({ post }) {
   const [commentBoxVisibility, setCommentBoxVisibility] = useState("hidden");
-  // const dispatch = useDispatch();
   function commentBoxHandler(event) {
     event.stopPropagation();
     if (commentBoxVisibility === "hidden") {
@@ -72,7 +67,6 @@ export function ShowPost({ post }) {
         }}
         id={post?._id}
         className={`${postParameters.postStyle}`}
-        // "p-2 pl-4  m-[1px] border-b   border-opacity-20 hover:bg-dark-hover cursor-pointer  "
       >
         <section className="flex justify-between ">
           <div className="flex justify-between">
@@ -119,18 +113,6 @@ export function ShowPost({ post }) {
                 }
                 className="   hover:bg-red hover:bg-opacity-30 rounded-full p-1"
               >
-                {/* <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  width="24"
-                  height="24"
-                >
-                  <path fill="none" d="M0 0H24V24H0z" />
-                  <path
-                    d="M12.001 4.529c2.349-2.109 5.979-2.039 8.242.228 2.262 2.268 2.34 5.88.236 8.236l-8.48 8.492-8.478-8.492c-2.104-2.356-2.025-5.974.236-8.236 2.265-2.264 5.888-2.34 8.244-.228zm6.826 1.641c-1.5-1.502-3.92-1.563-5.49-.153l-1.335 1.198-1.336-1.197c-1.575-1.412-3.99-1.35-5.494.154-1.49 1.49-1.565 3.875-.192 5.451L12 18.654l7.02-7.03c1.374-1.577 1.299-3.959-.193-5.454z"
-                    fill="rgba(149,164,166,1)"
-                  />
-                </svg> */}
                 <AiOutlineHeart size={24} />
               </button>
             ) : (
@@ -141,18 +123,6 @@ export function ShowPost({ post }) {
                 className=" text-red hover:bg-red hover:bg-opacity-30 rounded-full p-1"
               >
                 <AiFillHeart size={24} />
-                {/* <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  width="24"
-                  height="24"
-                >
-                  <path fill="none" d="M0 0H24V24H0z" />
-                  <path
-                    d="M12.001 4.529c2.349-2.109 5.979-2.039 8.242.228 2.262 2.268 2.34 5.88.236 8.236l-8.48 8.492-8.478-8.492c-2.104-2.356-2.025-5.974.236-8.236 2.265-2.264 5.888-2.34 8.244-.228z"
-                    fill="rgba(248,20,20,1)"
-                  />
-                </svg> */}
               </button>
             )}
             <p className="self-center ml-2">{buttonTransition.likes}</p>
