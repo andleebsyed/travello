225ec2cf24ca6489587108ec73b6d44ab106da5c
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-18 15:14:31 +0530

    update: added a logout button

--- src/common/pages/navbar/navbar.js
+++ src/common/pages/navbar/navbar.js
@@ -1,10 +1,19 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
 import { AiOutlineHome } from "react-icons/ai";
 import { RiSearchLine } from "react-icons/ri";
 import { IoIosNotificationsOutline } from "react-icons/io";
 import { IoPersonOutline } from "react-icons/io5";
+import { removeToken } from "../../../features/users/userSlice";
+import { useDispatch } from "react-redux";
 export function Navbar() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  function logoutHandler() {
+    localStorage.clear();
+    dispatch(removeToken());
+    navigate("/login");
+  }
   return (
     <main className=" fixed  xsm:sticky xsm:self-start w-screen bottom-0 xsm:top-0  xsm:pr-8 gridbreak:pr-0 xsm:w-auto bg-blue text-white  ">
       <ul className="flex  pr-4  justify-around xsm:flex-col xsm:justify-between items-end xsm:items-center gridbreak:items-start ">
@@ -52,11 +61,14 @@ export function Navbar() {
             <span className="navbar-list-item-name ">Profile</span>
           </NavLink>
         </li>
-        <li className="hidden xsm:block self-center mt-12">
-          <button className=" flex text-lg hover:bg-blue-xlight p-4 rounded-full hover:text-blue-light">
+        <li className="self-center gridbreak:mt-12">
+          <button
+            onClick={logoutHandler}
+            className=" flex text-lg hover:bg-blue-xlight p-4 rounded-full hover:text-blue-light"
+          >
             <p className="hidden gridbreak:block self-center mr-2">Logout</p>
             <div className="self-center">
-              <BiLogOut size={24} />
+              <BiLogOut size={30} />
             </div>
           </button>
         </li>
