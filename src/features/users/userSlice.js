647f32eba5e8b7c89730b75d0a1753deeea7e955
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-18 17:46:45 +0530

    update: added profile of user and the status of  api call to state

--- src/features/users/userSlice.js
+++ src/features/users/userSlice.js
@@ -1,7 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { FetchProfile } from "../../services/users/users";
+export const getUserProfile = createAsyncThunk("user", async (thunkAPI) => {
+  try {
+    const response = await FetchProfile();
+    console.log("thunk resposne", { response });
+    return response;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response);
+  }
+});
 export const userSlice = createSlice({
   name: "userSlice",
   initialState: {
+    profileStatus: "idle",
+    profile: null,
     status: "idle",
     error: null,
     authorized: localStorage.getItem("token") ? true : false,
@@ -14,7 +26,20 @@ export const userSlice = createSlice({
       state.authorized = false;
     },
   },
-  extraReducers: {},
+  extraReducers: {
+    [getUserProfile.pending]: (state) => {
+      state.profileStatus = "pending";
+    },
+    [getUserProfile.fulfilled]: (state, action) => {
+      const profile = action.payload.user;
+      state.profile = profile;
+      state.profileStatus = "success";
+    },
+    [getUserProfile.rejected]: (state, action) => {
+      state.profileStatus = "error";
+      state.error = action.payload;
+    },
+  },
 });
 export const { userAuth, setToken, removeToken } = userSlice.actions;
 export default userSlice.reducer;
