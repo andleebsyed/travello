40127f54f856a9192ecf42a66c36f38e4ae0e62d
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-21 22:19:26 +0530

    update: created some createAsyncThunk for different async operations

--- src/features/users/userSlice.js
+++ src/features/users/userSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { FetchProfile } from "../../services/users/users";
+import { FetchProfile, GetUser, LoadUsers } from "../../services/users/users";
 export const getUserProfile = createAsyncThunk("user", async (thunkAPI) => {
   try {
     const response = await FetchProfile();
@@ -9,13 +9,38 @@ export const getUserProfile = createAsyncThunk("user", async (thunkAPI) => {
     return thunkAPI.rejectWithValue(error.response);
   }
 });
+export const loadUsers = createAsyncThunk("/users/all", async (thunkAPI) => {
+  try {
+    const response = await LoadUsers();
+    console.log("thunk with all users ", { response });
+    return response;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response);
+  }
+});
+export const getUser = createAsyncThunk(
+  "/user/getuser",
+  async ({ getUserId }, thunkAPI) => {
+    try {
+      const response = await GetUser({ getUserId });
+      console.log({ response }, "user in thunk");
+      return response.user;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response);
+    }
+  }
+);
 export const userSlice = createSlice({
   name: "userSlice",
   initialState: {
+    fetchedUserProfile: null,
+    fetchUserProfileStatus: "idle",
     profileStatus: "idle",
     profile: null,
     status: "idle",
     error: null,
+    usersStatus: "idle",
+    users: null,
     authorized: localStorage.getItem("token") ? true : false,
   },
   reducers: {
@@ -44,6 +69,22 @@ export const userSlice = createSlice({
     [getUserProfile.rejected]: (state, action) => {
       state.profileStatus = "error";
       state.error = action.payload;
+    },
+    [loadUsers.fulfilled]: (state, action) => {
+      const { users } = action.payload;
+      state.users = users;
+      state.usersStatus = "success";
+    },
+    [loadUsers.pending]: (state) => {
+      state.usersStatus = "pending";
+    },
+    [loadUsers.rejected]: (state, action) => {
+      state.error = action.payload;
+      state.usersStatus = "error";
+    },
+    [getUser.fulfilled]: (state, action) => {
+      state.fetchedUserProfile = action.payload;
+      state.fetchUserProfileStatus = "success";
     },
   },
 });
