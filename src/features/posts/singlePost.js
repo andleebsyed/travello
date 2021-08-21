82384895f7452ae3ab49d53924071a20824afa39
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-21 22:16:25 +0530

    update: refactored some code and made view load on the based on a conditional which depends on the data present in concerned objects

--- src/features/posts/singlePost.js
+++ src/features/posts/singlePost.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { setUpAuthHeaderForServiceCalls } from "../../services/users/users";
@@ -7,25 +7,41 @@ import { ShowPost } from "./showPost";
 import { BiArrowBack } from "react-icons/bi";
 import { SpinnerLoader } from "../../common/components/Loaders/Spinner";
 import { getUserProfile } from "../users/userSlice";
-export function SinglePost() {
-  const { posts } = useSelector((state) => state.posts);
+import { GetPost } from "../../services/posts/posts";
+import { responsiveFontSizes } from "@material-ui/core";
+export function SinglePost({ post, user }) {
+  const { posts, status } = useSelector((state) => state.posts);
   const { postId } = useParams();
   const dispatch = useDispatch();
-  useEffect(() => {
-    if (posts === null || posts.length === 0) {
-      setUpAuthHeaderForServiceCalls(localStorage.getItem("token"));
-      dispatch(loadPosts());
-      dispatch(getUserProfile());
-    }
-  }, [dispatch, posts]);
-  const post = posts?.find((post) => post._id === postId);
+  const [postData, setPostData] = useState(null);
+  // useEffect(() => {
+  //   if (posts === null || posts.length === 0) {
+  //     setUpAuthHeaderForServiceCalls(localStorage.getItem("token"));
+  //     dispatch(loadPosts());
+  //     dispatch(getUserProfile());
+  //   }
+  // }, [dispatch, posts]);
   const navigate = useNavigate();
-  console.log({ post });
+  // console.log({ post });
   // useEffect(() => {});
+  // return (
+  // post === undefined ? (
+  //   <SpinnerLoader />
+  // ) : (
+  useEffect(() => {
+    async function Run() {
+      if (status !== "idle") {
+        console.log("useeffect ran");
+        const response = await GetPost({ postId });
+        console.log({ response });
+        const postDetails = { user: response.post.author, post: response.post };
+        setPostData(postDetails);
+        // setPostData(postData => {...postData, user: resposne.autho})}
+      }
+    }
+    Run();
+  }, [postId, status]);
   return (
-    // post === undefined ? (
-    //   <SpinnerLoader />
-    // ) : (
     <div className="w-screen min-h-screen md:w-[60vw] border border-opacity-10 xsm:mr-4 text-white">
       <section className="bg-blue  p-2 border-b border-opacity-20  flex">
         <button
@@ -36,7 +52,11 @@ export function SinglePost() {
         </button>
         <p className="p-2 text-xl">Post</p>
       </section>
-      {post === undefined ? <SpinnerLoader /> : <ShowPost post={post} />}
+      {postData === null ? (
+        <SpinnerLoader />
+      ) : (
+        <ShowPost post={postData.post} user={postData.user} />
+      )}
     </div>
   );
 }
