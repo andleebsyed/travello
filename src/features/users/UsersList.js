7dcf6aced2e14ff3ec64740f47e4ffb38f8ffdda
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-24 21:46:56 +0530

    update: added  links to serahced user's avatar and name to go to that user's profile when clicked

--- src/features/users/UsersList.js
+++ src/features/users/UsersList.js
@@ -29,7 +29,11 @@ export function UsersList({ users, searchResultsVisibility }) {
           {users?.map((user) => (
             <li className="flex flex-col">
               <section className="flex">
-                <Link to={`/user/${user._id}`}>
+                <Link
+                  to={
+                    profile?._id === user._id ? "/profile" : `/user/${user._id}`
+                  }
+                >
                   <img
                     alt="user avatar"
                     src={user?.avatar}
@@ -38,26 +42,37 @@ export function UsersList({ users, searchResultsVisibility }) {
                 </Link>
 
                 <section className="flex flex-col ml-2">
-                  <h1 className="font-bold hover:underline cursor-pointer">
-                    {user?.name}
-                  </h1>
+                  <Link
+                    to={
+                      profile?._id === user._id
+                        ? "/profile"
+                        : `/user/${user._id}`
+                    }
+                  >
+                    <h1 className="font-bold hover:underline cursor-pointer">
+                      {user?.name}
+                    </h1>
+                  </Link>
+
                   <h1>{user?.username}</h1>
                 </section>
-                {profile?.following.includes(user._id) ? (
-                  <button
-                    onClick={() => unfollowUserHandler(user._id)}
-                    className="self-center ml-auto rounded-3xl p-2  pb-3 border border-opacity-20 bg-blue-light text-white "
-                  >
-                    Following
-                  </button>
-                ) : (
-                  <button
-                    onClick={() => followUserHandler(user._id)}
-                    className=" ml-auto rounded-3xl p-2  pb-3 border border-blue-light border-opacity-20 hover:bg-blue-light hover:bg-opacity-20 text-white "
-                  >
-                    Follow
-                  </button>
-                )}
+                {profile?.following.includes(user._id)
+                  ? profile?._id !== user._id && (
+                      <button
+                        onClick={() => unfollowUserHandler(user._id)}
+                        className="self-center ml-auto rounded-3xl p-2  pb-3 border border-opacity-20 bg-blue-light text-white "
+                      >
+                        Following
+                      </button>
+                    )
+                  : profile._id !== user._id && (
+                      <button
+                        onClick={() => followUserHandler(user._id)}
+                        className=" ml-auto rounded-3xl p-2  pb-3 border border-blue-light border-opacity-20 hover:bg-blue-light hover:bg-opacity-20 text-white "
+                      >
+                        Follow
+                      </button>
+                    )}
               </section>
               <p className="font-bold   ml-2">{user.bio}</p>
             </li>
