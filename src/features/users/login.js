717ee69a7f54ab864abdc2cf11cc7400ff4b59bc
Syed Andleeb <andleebsyed75@gmail.com>
2021-10-03 11:34:23 +0530

    style: changed some text from link to p tag

--- src/features/users/login.js
+++ src/features/users/login.js
@@ -88,13 +88,10 @@ export function Login() {
               />
             </form>
             <section className="flex text-blue-light justify-center mt-4">
-              <Link to="#">
-                <button>New Here?</button>
-              </Link>
-
+              <p>New Here?</p>
               <span className="pl-1 pr-1">.</span>
               <Link to="/signup">
-                <button>Sign Up</button>
+                <button className="underline">Sign Up</button>
               </Link>
             </section>
             <button
